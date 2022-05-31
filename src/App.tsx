499ec94b674fd82739538d501206a8662ad9a2b2
jazzc001 <jazzcheung212@gmail.com>
2022-05-31 02:47:48 +0100

    showed the proper schedule

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,15 @@ function App() {
   interface Movie {
     id: number;
     name: string;
+    show: {
+      name: string;
+    }
 
   }
 
 
   useEffect(()=> {
-    fetch('https://api.tvmaze.com/schedule/web?date=2022-05-29&country=GB')
+    fetch('https://api.tvmaze.com/schedule?date=2022-05-30&country=GB')
     .then(response => response.json())
     .then(res => setMovies(res))
     .catch(err => setError(err));
@@ -25,7 +28,10 @@ function App() {
     <div className="App">
      
         <h1> Welcome to Movie World! </h1>
-        {movies.length > 0? movies.map((m: Movie)=> m.id) : ('loading...')}
+        {movies.length > 0? movies.map((m: Movie)=> {
+          return( <li>{m.show.name}, {m.id}</li>)
+         
+          }) : ('loading...')}
      
     </div>
   );
